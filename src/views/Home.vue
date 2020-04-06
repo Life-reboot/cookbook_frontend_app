e57260b70a5d57d606acdd45a55f8d47a273e0af
Peter Jang <peter.x.jang@gmail.com>
2020-04-06 18:49:02 -0500

    Display recipes from backend index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="recipe in recipes">
+      <h2>Title: {{ recipe.title }}</h2>
+      <p>Ingredients: {{ recipe.ingredients }}</p>
+      <p>Directions: {{ recipe.directions }}</p>
+    </div>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
 
 <script>
+var axios = require("axios");
+
 export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!!!!",
+      recipes: [],
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/recipes").then(response => {
+      this.recipes = response.data;
+    });
+  },
   methods: {},
 };
 </script>
