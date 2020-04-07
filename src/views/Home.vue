773c2e7e14f338ff44a0f822e5e1481f933b7811
Peter Jang <peter.x.jang@gmail.com>
2020-04-06 20:47:15 -0500

    Add button to show details of each item (hidden by default)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,10 +20,13 @@
     <h1>All recipes</h1>
     <div v-for="recipe in recipes">
       <h2>Title: {{ recipe.title }}</h2>
-      <p>Image: {{ recipe.image_url }}</p>
-      <img v-bind:src="recipe.image_url" alt="" />
-      <p>Ingredients: {{ recipe.ingredients }}</p>
-      <p>Directions: {{ recipe.directions }}</p>
+      <button v-on:click="currentRecipe = recipe">Show more info</button>
+      <div v-if="recipe === currentRecipe">
+        <p>Image: {{ recipe.image_url }}</p>
+        <img v-bind:src="recipe.image_url" alt="" />
+        <p>Ingredients: {{ recipe.ingredients }}</p>
+        <p>Directions: {{ recipe.directions }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -42,6 +45,7 @@ export default {
     return {
       message: "Welcome to Vue.js!!!!",
       recipes: [],
+      currentRecipe: {},
       newRecipeTitle: "",
       newRecipeChef: "",
       newRecipePrepTime: "",
